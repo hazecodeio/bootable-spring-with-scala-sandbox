42ec82452f830687c22ffee854286b7475fd8ac7
hsmak <husain.alkhamis@gmail.com>
2020-12-13 22:15:43 -0600

    committing changes..

--- src/main/scala/boot/ScalaBootApp.scala
+++ src/main/scala/boot/ScalaBootApp.scala
@@ -1,14 +1,20 @@
 package boot
 
-import org.hsmak.MyJava
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.{GetMapping, RestController}
 
 @SpringBootApplication
 class ScalaBootApp
 
 object ScalaBootApp extends App {
-  println("This String is printed from scala method!")
-  println(MyJava.callJavaMethod)
-  SpringApplication.run(classOf[ScalaBootApp], args:_*)
+  SpringApplication.run(classOf[ScalaBootApp], args: _*)
+}
+
+@RestController
+class MyController {
+  @GetMapping(Array("/"))
+  def stringGetRequest = {
+    "This is a string for Scala-based Controller.."
+  }
 }
